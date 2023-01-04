7e383378e2cb81892023a544eb7af6480f7b5d1f
Jon <jonbp42@gmail.com>
2023-01-03 19:24:53 -0800

    reupload

---
+++ src/constants/index.js
@@ -0,0 +1,197 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Fully Programmable",
+    content:
+      "Powered by two Arduino Pro micros, this keyboard is compatible with QMK.",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "Ergonomic",
+    content:
+      "Each key is position at the optimal depth and height rather than conforming your hands to the unnatural staggered grid layout.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Affordable",
+    content:
+      "Save money by building your own. Cheaper than an average mechanical keyboard and compatible with any switch.",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "The endgame is here.",
+    name: "Jon B.",
+    title: "Founder & Leader",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Ditch the mouse for good.",
+    name: "Kyrillos Clemens",
+    title: "Founder & Leader",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "After an adjustment period, there is no going back.",
+    name: "Myron Sanford",
+    title: "Customer",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Reviews",
+    value: "133",
+  },
+  {
+    id: "stats-2",
+    title: "Variants",
+    value: "12+",
+  },
+  {
+    id: "stats-3",
+    title: "Starting",
+    value: "$99",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Docs",
+        link: "https://docs.qmk.fm/#/",
+      },
+      {
+        name: "How it Works",
+        link: "https://jbandp.github.io/how-it-works/",
+      },
+      {
+        name: "Create",
+        link: "https://jbandp.github.io/create/",
+      },
+      {
+        name: "Affiliates",
+        link: "https://jbandp.github.io/affiliates/",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://jbandp.github.io/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://jbandp.github.io/help-center/",
+      },
+      {
+        name: "Discord",
+        link: "https://discord.com/invite/ARst6hfwkk",
+      },
+      {
+        name: "Suggestions",
+        link: "https://jbandp.github.io/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://jbandp.github.io/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://jbandp.github.io/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Builders",
+        link: "https://jbandp.github.io/our-builders/",
+      },
+      {
+        name: "Become a Builder",
+        link: "https://jbandp.github.io/become-a-builder/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
